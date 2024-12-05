c016ebe6e1c6c1b6a0800e853db03df9e6d906cc
espressotoexpressions <keannevillar@gmail.com>
2024-12-05 10:11:42 -0800

    created daily functionality to display daily data

--- scripts/app.js
+++ scripts/app.js
@@ -1,47 +1,46 @@
-let activitiesList = document.getElementById("activitiesList");
-let current = document.getElementById("current");
+let workCurrentTxt = document.getElementById("workCurrentTxt");
+let workPreviousTxt=document.getElementById("workPreviousTxt");
+let dailyLink =document.getElementById("dailyLink");
+let weeklyLink= document.getElementById("weeklyLink");
+let monthlyLink= document.getElementById("monthlyLink");
+
 function getAllData() {
      return fetch("../data/data.json")
         .then(response => response.json())
         .then(data => {
-
-            console.log("FULL JSON DATA");
-            console.log(data);
             return data;
 
-        })
+        });
 }
 
 function filterData(activityType) {
 
     let filteredActivityList=[];
     return getAllData().then(data => {
-        //go through each item in the data
+        //go through each item in the data and add  to another array depending on the activityType passed
         data.map(item => {
-
             switch (activityType) {
                 case "daily":
-                    // console.log("entered daily");
                     filteredActivityList.push({
                         'title': item.title,
                         'current': item.timeframes.daily.current,
                         'previous': item.timeframes.daily.previous
                     });
-                    
-
                     break;
 
                 case "weekly":
                     filteredActivityList.push({
-                        title: item.title,
-                        weekly: item.timeframes.weekly
+                        'title': item.title,
+                        'current': item.timeframes.weekly.current,
+                        'previous': item.timeframes.weekly.previous
                     });
                     break;
 
                 case "monthly":
                     filteredActivityList.push({
-                        title: item.title,
-                        monthly: item.timeframes.monthly
+                        'title': item.title,
+                        'current': item.timeframes.monthly.current,
+                        'previous': item.timeframes.monthly.previous
                     });
                     break;
 
@@ -50,46 +49,48 @@ function filterData(activityType) {
 
             }// end of switch statment
 
-        });
-
-        
+        });   
             return (filteredActivityList);
-
     });
 
 }
 
-
-function displayCards() {
-    let activityList = filterData('daily');
-    // console.log('FILTERED DATA');
-    // console.log(`LENGTH : ${activityList.length}`);
-    // console.log(`activityList type:  ${typeof activityList}`);
-    // console.log(activityList);
-
-    // activityList.map(item =>{
-
-        
-    //         console.log("ENTER ACTIVITYLIST");
-    //         console.log(item.timeframes);
-    //         // console.log(`DAILY  CURRENT: ${activityList[i].timeframes.daily.current}`);
-    // })
+// displayData based on activityType
+function displayCards(activityType) {
     let p =document.createElement('p');
-    
-    filterData('daily').then(data => {
-        console.log("FILTERED");
-        console.log(data);
+        filterData(activityType).then(data => {
         data.map( item=>{
-            let h1 =document.createElement('h1');
-            h1.innerText=item.title;
-            activitiesList.appendChild(h1)
+  
+            
+            switch (item.title){
+                case "Work":
+                    workCurrentTxt.innerText=`${item.current} hrs`;
+                    workPreviousTxt.innerText=`Last Week - ${item.previous}hrs`;
+                    break;
+                case "Play":
+                        playCurrentTxt.innerText=`${item.current} hrs`;
+                        playPreviousTxt.innerText=`Last Week - ${item.previous}hrs`;
+                        break;
+                case "Study":
+                        studyCurrentTxt.innerText=`${item.current} hrs`;
+                        studyPreviousTxt.innerText=`Last Week - ${item.previous}hrs`;
+                        break;
+                case "Exercise":
+                        exerciseCurrentTxt.innerText=`${item.current} hrs`;
+                        exercisePreviousTxt.innerText=`Last Week - ${item.previous}hrs`;
+                        break;
+            case "Social":
+                        socialCurrentTxt.innerText=`${item.current} hrs`;
+                        socialPreviousTxt.innerText=`Last Week - ${item.previous}hrs`;
+                        break;
 
-            let p =document.createElement('p');
-            p.innerText=item.current;
-            current.appendChild(p);
+             case "Self Care":
+                        selfcareCurrentTxt.innerText=`${item.current} hrs`;
+                        selfcarePreviousTxt.innerText=`Last Week - ${item.previous}hrs`;
+                        break;
+                default: console.log("Invalid item title");
 
-            console.log(`CURRENT ${item.current}`);
-            console.log(`TIME: ${item.title}`);
+            }
         }
 
         )
@@ -98,4 +99,20 @@ function displayCards() {
    
 }
 
-displayCards();
+//display daily as default
+displayCards('daily'); 
+
+//display data based on the link clicked
+dailyLink.addEventListener('click',function(){
+console.log("Enter daily");
+    displayCards('daily');
+});
+
+weeklyLink.addEventListener('click',function(){
+    console.log("Enter weekly");
+        displayCards('weekly');
+    });
+monthlyLink.addEventListener('click',function(){
+    console.log("Enter monthly");
+        displayCards('monthly');
+    });
